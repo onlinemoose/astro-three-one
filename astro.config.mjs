163c4d58bbce70df15e7a5621a68abcdcae28098
Markus Smet <markussmet@hotmail.com>
2024-04-27 15:52:14 +0200

    Initial Commit 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+import db from "@astrojs/db";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [db()]
+});
