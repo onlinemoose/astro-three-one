1b0048825441f5061bfc56cbeb40b5b81cb400c2
Markus Smet <markussmet@hotmail.com>
2024-04-28 00:21:37 +0200

    Some commit

--- db/config.ts
+++ db/config.ts
@@ -19,7 +19,7 @@ const Link = defineTable({
     url: column.text(),
     description: column.text(),
     isRead: column.boolean({ default: false }),
-    customerId: column.number()
+    customerId: column.number({ default: 0 })
     // customerId: column.number({ references: () => Customer.columns.id })
   }
 })
