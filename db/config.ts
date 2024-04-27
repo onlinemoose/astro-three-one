7f9d8f4c18fd948f517eb666d484fd17d37551f3
Markus Smet <markussmet@hotmail.com>
2024-04-28 00:12:33 +0200

    Change to table structure

--- db/config.ts
+++ db/config.ts
@@ -1,17 +1,5 @@
 import { column, defineDb, defineTable } from 'astro:db';
 
-const Link = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    title: column.text(),
-    url: column.text(),
-    description: column.text(),
-    isRead: column.boolean({ default: false }),
-    customerId: column.number()
-    // customerId: column.number({ references: () => Customer.columns.id })
-  }
-})
-
 const Customer = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
@@ -23,6 +11,18 @@ const Customer = defineTable({
     zipCode: column.text()
   }
 });
+
+const Link = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text(),
+    url: column.text(),
+    description: column.text(),
+    isRead: column.boolean({ default: false }),
+    customerId: column.number()
+    // customerId: column.number({ references: () => Customer.columns.id })
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
--- db/seed.ts
+++ db/seed.ts
@@ -2,6 +2,32 @@ import { Customer, Link, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
+	await db.insert(Customer).values([
+		{
+			firstName: "John",
+			lastName: "Doe",
+			street: "123 Main St",
+			city: "New York",
+			state: "NY",
+			zipCode: "10001"
+		},
+		{
+			firstName: "Jane",
+			lastName: "Smith",
+			street: "456 Elm St",
+			city: "San Francisco",
+			state: "CA",
+			zipCode: "94101"
+		},
+		{
+			firstName: "Mark",
+			lastName: "Johnson",
+			street: "789 Oak St",
+			city: "Seattle",
+			state: "WA",
+			zipCode: "98101"
+		}
+	]);
 	await db.insert(Link).values([
 		{
 			title: "My Blog",
@@ -23,35 +49,6 @@ export default async function seed() {
 			description: "I found this cool site that can hold repositories.",
 			isRead: false,
 			customerId: 3
-		}
-	]);
-	await db.insert(Customer).values([
-		{
-			id: 1,
-			firstName: "John",
-			lastName: "Doe",
-			street: "123 Main St",
-			city: "New York",
-			state: "NY",
-			zipCode: "10001"
-		},
-		{
-			id: 2,
-			firstName: "Jane",
-			lastName: "Smith",
-			street: "456 Elm St",
-			city: "San Francisco",
-			state: "CA",
-			zipCode: "94101"
-		},
-		{
-			id: 3,
-			firstName: "Mark",
-			lastName: "Johnson",
-			street: "789 Oak St",
-			city: "Seattle",
-			state: "WA",
-			zipCode: "98101"
 		}
 	]);
 }
